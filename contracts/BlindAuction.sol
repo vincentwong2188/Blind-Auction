b1add81e8ca1eca31fbcde6d3a580891579c46b4
Zhi Qi Sim <zhiqisim@gmail.com>
2020-10-20 16:43:04 +0800

    init blind auction

---
+++ contracts/BlindAuction.sol
@@ -0,0 +1,181 @@
+pragma solidity >0.4.23 <0.7.0;
+
+contract BlindAuction {
+    // struct Bid {
+    //     // bytes32 blindedBid;
+    //     bytes32[] blindedBids;
+    //     uint deposit;
+    // }
+
+
+    // TODO: ADD IN DEPOSIT MAPPING
+    // TODO: API DOC
+
+    address payable public beneficiary;
+    uint public biddingEnd;
+    uint public revealEnd;
+    bool public ended;
+
+    // mapping(address => Bid[]) public bids;
+    mapping(address => bytes32[]) public bids;
+    mapping(address => uint) deposits;
+
+    address public highestBidder;
+    uint public highestBid;
+
+    // Allowed withdrawals of previous bids
+    mapping(address => uint) pendingReturns;
+
+    event AuctionEnded(address winner, uint highestBid);
+
+    event BidCreated(bytes32 bidHash, uint deposit, address bidder);
+
+    event ProcessReveal(uint deposits);
+
+    event RevealHashes(bytes32 original, bytes32 test);
+
+    /// Modifiers are a convenient way to validate inputs to
+    /// functions. `onlyBefore` is applied to `bid` below:
+    /// The new function body is the modifier's body where
+    /// `_` is replaced by the old function body.
+    modifier onlyBefore(uint _time) { require(now < _time); _; }
+    modifier onlyAfter(uint _time) { require(now > _time); _; }
+
+    constructor(
+        uint _biddingTime,
+        uint _revealTime
+        // address payable _beneficiary
+    ) payable public {
+        beneficiary = msg.sender;
+        biddingEnd = now + _biddingTime;
+        revealEnd = biddingEnd + _revealTime;
+    }
+
+    /// Place a blinded bid with `_blindedBid` =
+    /// keccak256(abi.encodePacked(value, fake, secret)).
+    /// The sent ether is only refunded if the bid is correctly
+    /// revealed in the revealing phase. The bid is valid if the
+    /// ether sent together with the bid is at least "value" and
+    /// "fake" is not true. Setting "fake" to true and sending
+    /// not the exact amount are ways to hide the real bid but
+    /// still make the required deposit. The same address can
+    /// place multiple bids.
+    function bid(
+        bytes32 _blindedBid,
+        address _bidder)
+        public
+        payable
+        onlyBefore(biddingEnd)
+    {
+        require(_blindedBid.length > 0);
+        bids[_bidder].push(_blindedBid);
+        deposits[_bidder] += msg.value
+        emit BidCreated(bids[_bidder][bids[_bidder].length-1], deposits[_bidder]);
+    }
+
+    // Reveal bids to verify bids that were sent by the user,
+    // this will effectively allow the user to test if he has 
+    // the highest bid by verifying he actually sent it.
+    // Bids has to be sent in order of bidding for reveal
+    // TODO: ONLY ALLOW a user to reveal only
+    function reveal(
+        uint[] memory _values,
+        bool[] memory _fake,
+        bytes32[] memory _secret,
+        bytes32 _hash
+    )
+        public
+        onlyAfter(biddingEnd)
+        onlyBefore(revealEnd)
+        returns (bool isValid)
+    {
+        isValid = true
+        uint length = bids[msg.sender].length;
+        require(_values.length == length);
+        require(_fake.length == length);
+        require(_secret.length == length);
+        // uint refund;
+        for (uint i = 0; i < length; i++) {
+            bytes32 storage bidToCheck = bids[msg.sender][i];
+            
+            (uint value, bool fake, bytes32 secret) =
+                    (_values[i], _fake[i], _secret[i]);
+            // TODO: FIX hash difference in JS and here
+            emit RevealHashes(bidToCheck.blindedBid, _hash);
+            // if (bidToCheck.blindedBid != keccak256(abi.encodePacked(value, fake, secret))) {
+            if (bidToCheck.blindedBid != _hash) {
+                // Bid was not actually revealed.
+                // Do not refund deposit.
+                isValid = false
+                continue;
+            }
+            if (!fake && bids[msg.sender] >= value) {
+                if (placeBid(msg.sender, value))
+                    deposits[msg.sender] -= value;
+            }
+            // Make it impossible for the sender to re-claim
+            // the same deposit.
+            bids[msg.sender][i] = bytes32(0);
+        }
+        // return all deposits to user 
+        pendingReturns[msg.sender] += deposits[msg.sender];
+        emit ProcessReveal(bids[msg.sender]);
+        // _bidder.transfer(refund);
+        return isValid
+        
+    }
+
+    // This is an "internal" function which means that it
+    // can only be called from the contract itself (or from
+    // derived contracts).
+    function placeBid(address bidder, uint value) internal
+            returns (bool success)
+    {
+        if (value <= highestBid) {
+            return false;
+        }
+        if (highestBidder != address(0)) {
+            // Refund the previously highest bidder to deposits.
+            pendingReturns[highestBidder] += highestBid;
+        }
+        highestBid = value;
+        highestBidder = bidder;
+        return true;
+    }
+
+
+
+    /// End the auction and send the highest bid
+    /// to the beneficiary.
+    // refund everyone's deposit using withdraw call 
+    function auctionEnd()
+        public
+        onlyAfter(revealEnd)
+        returns (address)
+    {
+        require(!ended);
+        emit AuctionEnded(highestBidder, highestBid);
+        ended = true;
+        beneficiary.transfer(highestBid);
+        return highestBidder;
+    }
+
+    /// Withdraw a bid that was overbid.
+    // called after auction end
+    function withdraw(
+        // address payable _bidder
+    ) public returns (uint amount) {
+        require(ended);
+        amount = pendingReturns[msg.sender];
+        if (amount > 0) {
+            // It is important to set this to zero because the recipient
+            // can call this function again as part of the receiving call
+            // before `transfer` returns (see the remark above about
+            // conditions -> effects -> interaction).
+            pendingReturns[msg.sender] = 0;
+
+            msg.sender.transfer(amount);
+        }
+        return amount;
+    }
+}
---
+++ test/BlindAuction.js
@@ -0,0 +1,88 @@
+const { assert } = require('chai')
+
+const { soliditySha3, toWei, fromAscii } = require("web3-utils");
+
+const BlindAuction = artifacts.require('./BlindAuction.sol')
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+  // accounts are test accounts on local network
+  contract('BlindAuction', ([deployer, bidder1, bidder2]) => {
+      let blindAuction
+
+      before(async () => {
+        blindAuction = await BlindAuction.deployed(1, 1)
+      })
+
+      describe('deployment', async () => {
+          it('deploys successfully', async () => {
+            const address = await blindAuction.address
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+          })
+
+          it('has bidding time', async () => {
+            const biddingEnd = await blindAuction.biddingEnd()
+            assert.notEqual(biddingEnd, 0)
+          })
+
+          it('has reveal time', async () => {
+            const revealEnd = await blindAuction.revealEnd()
+            assert.notEqual(revealEnd, 0)
+          })
+      })
+
+      describe('bids', async () => {
+        let result1
+        let result2
+        let hashBid1
+        let hashBid2
+        before(async () => {
+          // use URL below for keccak256 hash in JS
+          // https://blog.8bitzen.com/posts/18-03-2019-keccak-abi-encodepacked-with-javascript/
+          hashBid1 = soliditySha3(
+            1,
+            false,
+            fromAscii("secret")
+          );
+          // remember change secret to bytes
+          hashBid2 = soliditySha3(
+            2,
+            false,
+            fromAscii("secret")
+          );
+          result1 = await blindAuction.bid(hashBid1, bidder1, {from: deployer, value: toWei("1")})
+          result2 = await blindAuction.bid(hashBid2, bidder2, {from: deployer, value: toWei("2")})
+        })
+        it('send bid', async () => {
+          
+          const event = result1.logs[0].args
+          assert.equal(event.bidHash, hashBid1, 'Hashed bid is correct')
+          assert.equal(event.deposit, toWei("1"), 'Deposit is correct')
+          assert.equal(event.bidder, bidder1, 'bidder is correct')
+          // FAILURE: bid must have blinded bid content
+          await blindAuction.bid("", bidder1).should.be.rejected;
+        
+        })
+
+        it('reveal bid', async () => {
+          const reveal = await blindAuction.reveal([1], [false], [fromAscii("secret")],hashBid1, bidder1)
+          const reveal2 = await blindAuction.reveal([2], [false], [fromAscii("secret")], hashBid2, bidder2)
+          const event = reveal.logs[0].args
+          const event2 = reveal2.logs[0].args
+          // assert.equal(event.refund, 1, 'Amount refunded correct')
+          // assert.equal(event2.refund, 0, 'Amount of highest bidder not refunded')
+        })
+
+        it('auction end', async () => {
+          const end = await blindAuction.auctionEnd()
+          const event = end.logs[0].args
+          assert.equal(event.winner, bidder2, 'Winner of auction is correct')
+          assert.equal(event.highestBid, 2, 'Highest bid of auction is correct')
+        })
+      })
+  })
