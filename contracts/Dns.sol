a0f2ebb07dc74220885f85ba821aa7805ece11a0
Shen Chen <shenc909@users.noreply.github.com>
2020-10-27 20:02:59 +0800

    Added test functions

--- contracts/Dns.sol
+++ contracts/Dns.sol
@@ -70,15 +70,19 @@ contract Dns {
 
         AuctionItem memory new_auction;
         // Init new auction here
-        new_auction.auction = createAuction();
+        new_auction.auction = createAuction(url);
         new_auction.addr = address(new_auction.auction);
         new_auction.bidding_end = new_auction.auction.biddingEnd();
         auctions[url] = new_auction;
         emit AuctionStart(new_auction.addr, url);
     }
 
-    function createAuction() private returns (BlindAuction) {
-        BlindAuction auction = new BlindAuction(bidding_length, reveal_length);
+    function createAuction(string memory url) private returns (BlindAuction) {
+        BlindAuction auction = new BlindAuction(
+            bidding_length,
+            reveal_length,
+            url
+        );
         return auction;
     }
 
@@ -167,5 +171,13 @@ contract Dns {
             internalAddressRegister(url, addr);
             emit Registration(addr, url, expiry_date[url]);
         }
+    }
+
+    function testFuncParam(int256 input_int) public pure returns (int256) {
+        return input_int;
+    }
+
+    function testFunc() public pure returns (string memory) {
+        return ("teststring.ntu");
     }
 }
