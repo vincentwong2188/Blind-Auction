7f0392698b0e96a2ce907e76d67f10acfbae7d2d
Vincent Wong <vincentwong.yong.sheng.2011@vjc.sg>
2020-11-02 23:16:36 +0800

    added more assets

--- webapp/configurations.js
+++ webapp/configurations.js
@@ -1,2 +1,2 @@
-export const ENVIRONMENT = 'GOERLI'; // GANACHE, ROPSTEN, or GOERLI
+export const ENVIRONMENT = 'GANACHE'; // GANACHE, ROPSTEN, or GOERLI
 export const DnsContractAddressGanache = '0xA59960d719799a9D45566f5b068EfB2a75F06611';
